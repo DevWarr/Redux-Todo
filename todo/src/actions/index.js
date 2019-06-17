0cd6d1c4eac5ef5a843a8722b193533c64be2745
Devin Warrick <dev.warr@gmail.com>
2019-06-17 15:28:35 -0500

    Five actions now declared: add, delete, deleteAll, deleteSelected, toggle.

--- todo/src/actions/index.js
+++ todo/src/actions/index.js
@@ -1,5 +1,7 @@
 export const ADD_TODO = "ADD_TODO";
 export const DELETE_TODO = "DELETE_TODO";
+export const DELETE_ALL = "DELETE_ALL";
+export const DELETE_SELECTED = "DELETE_SELECTED";
 export const TOGGLE_TODO = "TOGGLE_TODO";
 
 
@@ -7,12 +9,31 @@ export const addTodo = todoObj => {
     return {
         type: ADD_TODO,
         payload: todoObj
-    };
+    }
 }
 
 export const deleteTodo = todoId => {
     return {
         type: DELETE_TODO,
         payload: todoId
-    };
+    }
+}
+
+export const deleteAll = () => {
+    return {
+        type: DELETE_ALL
+    }
+}
+
+export const deleteSelected = () => {
+    return {
+        type: DELETE_SELECTED
+    }
+}
+
+export const toggleTodo = todoId => {
+    return {
+        type: TOGGLE_TODO,
+        payload: todoId
+    }
 }
